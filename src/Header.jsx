beb51452330e5f195a17ee6b1d1016ea29256fb4
vaibhav <fundevaibhav02@gmail.com>
2026-01-01 13:48:33 +0530

    N

--- src/Header.jsx
+++ src/Header.jsx
@@ -87,7 +87,7 @@ function Header() {
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/menu">Menu</Link></li>
                 <li><Link to="/about">About</Link></li>
-               
+                <li><Link to="/contact">Contact</Link></li>
                 <li><Link to="/rating">Rating</Link></li>
                 <li>
                   <Link to="/cart" className="order-btn">
